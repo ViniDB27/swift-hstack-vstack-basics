78e8ac21bf648e8f70ab84c5c08b86b676c41dde
Vinicio Brejinski <viniciosdb@gmail.com>
2023-02-24 15:19:14 -0300

    Initial Commit

---
+++ hstack-vstack-basics/hstack_vstack_basicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hstack_vstack_basicsApp.swift
+//  hstack-vstack-basics
+//
+//  Created by Vinicio Brejinski on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct hstack_vstack_basicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
